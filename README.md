d1f65e165e6d41521af62e3ecc80822482fcdff8
farhanacsebd <farhana.cse.bd@gmail.com>
2022-11-25 02:37:40 +0600

    added change btn

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 ## BackgrounGradient Generator
+
 `before copy button the code is:`
+
 ```javascript
-const css = document.getElementById('output');
-const color1 = document.getElementById('color1');
-const color2 = document.getElementById('color2');
-const color3 = document.getElementById('color3');
-const body = document.getElementById('gradient');
+const css = document.getElementById("output");
+const color1 = document.getElementById("color1");
+const color2 = document.getElementById("color2");
+const color3 = document.getElementById("color3");
+const body = document.getElementById("gradient");
 
-
-function setGradient(){
-    body.style.background =`linear-gradient(to right, ${color1.value} ,${color2.value},${color3.value}`;
-    css.value = `${body.style.background}`;
+function setGradient() {
+  body.style.background = `linear-gradient(to right, ${color1.value} ,${color2.value},${color3.value}`;
+  css.value = `${body.style.background}`;
 }
 
-color1.addEventListener('input',setGradient) 
-color2.addEventListener('input',setGradient)
-color3.addEventListener('input',setGradient)
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+color3.addEventListener("input", setGradient);
 ```
--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const css = document.getElementById('output');
-const color1 = document.getElementById('color1');
-const color2 = document.getElementById('color2');
-const color3 = document.getElementById('color3');
-const body = document.getElementById('gradient');
+const css = document.getElementById("output");
+const color1 = document.getElementById("color1");
+const color2 = document.getElementById("color2");
+const color3 = document.getElementById("color3");
+const body = document.getElementById("gradient");
+const changeBtn = document.getElementById("change-btn");
 
-
-function setGradient(){
-    body.style.background =`linear-gradient(to right, ${color1.value} ,${color2.value},${color3.value}`;
-    css.value = `${body.style.background}`;
+function setGradient() {
+  body.style.background = `linear-gradient(to right, ${color1.value} ,${color2.value},${color3.value}`;
+  css.value = `${body.style.background}`;
 }
 
-document.getElementById('copy-btn').addEventListener('click',function(){
+document.getElementById("copy-btn").addEventListener("click", function () {
+  navigator.clipboard.writeText(css.value);
+});
 
-    navigator.clipboard.writeText(css.value)
-  
-  })
-
-color1.addEventListener('input',setGradient) 
-color2.addEventListener('input',setGradient)
-color3.addEventListener('input',setGradient)
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+color3.addEventListener("input", setGradient);
