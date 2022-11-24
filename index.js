e888d1fc3c4b31d0a391edd89cde5ab4f83d8c00
farhanacsebd <farhana.cse.bd@gmail.com>
2022-11-25 01:11:19 +0600

    added js function

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const css = document.getElementsByClassName('h3');
 const color1 = document.getElementById('color1');
 const color2 = document.getElementById('color2');
-const body = document.getElementById('color2');
+const body = document.getElementById('gradient');
 
-color1.addEventListener('click',function(){
-    console.log(color1.value);
-});
 
-color2.addEventListener('click',function(){
-    console.log(color2.value);
-})
+function setGradient(){
+    body.style.background =`linear-gradient(to right, ${color1.value} ,${color2.value}`;
+}
+
+color1.addEventListener('input',setGradient) 
+color2.addEventListener('input',setGradient)
