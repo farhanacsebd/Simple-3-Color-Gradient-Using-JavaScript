9f4896c07e538688ddc3387ecaa429ff8737bb7a
farhanacsebd <farhana.cse.bd@gmail.com>
2022-11-24 23:48:57 +0600

    added js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello');
+const css = document.getElementsByClassName('h3');
+const color1 = document.getElementById('color1');
+const color2 = document.getElementById('color2');
+const body = document.getElementById('color2');
+
+color1.addEventListener('click',function(){
+    console.log(color1.value);
+});
+
+color2.addEventListener('click',function(){
+    console.log(color2.value);
+})
