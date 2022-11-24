73f5e63939be378f330c2a4efc76231be0b1ee5b
farhanacsebd <farhana.cse.bd@gmail.com>
2022-11-25 01:39:23 +0600

    added button color js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const css = document.getElementsByClassName('h3');
+const css = document.querySelector('h3');
 const color1 = document.getElementById('color1');
 const color2 = document.getElementById('color2');
+const color3 = document.getElementById('color3');
 const body = document.getElementById('gradient');
 
 
 function setGradient(){
-    body.style.background =`linear-gradient(to right, ${color1.value} ,${color2.value}`;
+    body.style.background =`linear-gradient(to right, ${color1.value} ,${color2.value},${color3.value}`;
+    css.innerHTML = `${body.style.background}`;
 }
+
 
 color1.addEventListener('input',setGradient) 
 color2.addEventListener('input',setGradient)
+color3.addEventListener('input',setGradient)
