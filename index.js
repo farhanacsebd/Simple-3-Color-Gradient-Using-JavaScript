c0fe34acf63c53c38b0a3e922566ffac91d6b956
farhanacsebd <farhana.cse.bd@gmail.com>
2022-11-25 23:46:20 +0600

    added button task

--- index.js
+++ index.js
@@ -14,6 +14,31 @@ document.getElementById("copy-btn").addEventListener("click", function () {
   navigator.clipboard.writeText(css.value);
 });
 
+function generateColor() {
+//hex calculating  
+  let hexCodes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    let num = Math.round(Math.random() * 15);
+    let num2 = hexCodes[num];
+    color += num2;
+    // console.log(color);
+  }
+  return color;
+}
+
+
+function setGenerateColor() {
+// it is generate with outher input
+  color1.value = generateColor();
+  color2.value = generateColor();
+  color3.value = generateColor();
+  setGradient();
+}
+
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 color3.addEventListener("input", setGradient);
+changeBtn.addEventListener("click", setGenerateColor);   //call the button 
